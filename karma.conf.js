4e0b975ac97fff15cd2de87f4b4bdb7d82667b6f
hyurii <curious725@gmail.com>
2018-07-06 18:19:02 +0300

    add support of headless firefox

--- karma.conf.js
+++ karma.conf.js
@@ -58,8 +58,16 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
+    browsers: ['Firefox','FirefoxHeadless'],
 
+    customLaunchers: {
+      'FirefoxHeadless': {
+        base: 'Firefox',
+        flags: [
+          '-headless',
+        ],
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
